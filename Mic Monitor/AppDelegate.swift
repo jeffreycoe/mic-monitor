d50fd81f6740588d9c92dea9256a71a36b7a9d02
Jeffrey Coe <jeffrey.coe@cerner.com>
2016-10-22 17:10:57 -0400

    Initial Commit

---
+++ Mic Monitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mic Monitor
+//
+//  Created by Jeffrey Coe on 10/22/16.
+//  Copyright © 2016 Jeffrey Coe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mic Monitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mic Monitor
+//
+//  Created by Jeffrey Coe on 10/22/16.
+//  Copyright © 2016 Jeffrey Coe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
